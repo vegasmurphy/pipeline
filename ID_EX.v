cd463ce4bd8dbc401decb010f3dfd36760c1b616
vegasmurphy <vegasmurphy@hotmail.com>
2014-03-31 16:33:05 -0300

    MEM_WB

--- ID_EX.v
+++ ID_EX.v
@@ -24,17 +24,17 @@ module ID_EX(
     input [31:0] Read_Data_2_ID,
     input [31:0] signExtended_ID,
 	 input [31:0] PC_sumado_ID,
-	 output [31:0] PC_sumado_EX,
-    output [31:0] Read_Data_1_EX,
-    output [31:0] Read_Data_2_EX,
-    output [31:0] signExtended_EX
+	 output reg [31:0] PC_sumado_EX,
+    output reg [31:0] Read_Data_1_EX,
+    output reg [31:0] Read_Data_2_EX,
+    output reg [31:0] signExtended_EX
     );
 always @(posedge clk)
 begin
-	Read_Data_1_ID=Read_Data_1_EX;
-	Read_Data_2_ID=Read_Data_2_EX;
-	signExtended_ID=signExtended_EX;
-	PC_sumado_ID=PC_sumado_EX;
+	Read_Data_1_EX=Read_Data_1_ID;
+	Read_Data_2_EX=Read_Data_2_ID;
+	signExtended_EX=signExtended_ID;
+	PC_sumado_EX=PC_sumado_ID;
 end
 
 endmodule
---
+++ MEM_WB.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:28:05 03/31/2014 
+// Design Name: 
+// Module Name:    MEM_WB 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module MEM_WB(
+	 input clk,
+    input [31:0] Read_data_MEM,
+    input [31:0] ALU_result_MEM,
+    output reg [31:0] Read_data_WB,
+    output reg [31:0] ALU_result_WB
+    );
+always @(posedge clk)
+begin
+	Read_data_WB=Read_data_MEM;
+	ALU_result_WB=ALU_result_MEM;
+end
+
+endmodule
